9791221c6e94c614e118ccd754d100c054152a0a
samarasrs <smr.samara.srs@gmail.com>
2022-10-24 02:47:56 -0300

    Alimentando bd

--- habitas/main/admin.py
+++ habitas/main/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Tree
 
-# Register your models here.
 
-admin.site.register(Tree)
+class TreeResource(resources.ModelResource):
+    class Meta:
+        model = Tree
+        import_id_fields = ("N_placa",)
+        fields = ["N_placa", "nome_popular", "nome_cientifico", "dap", "altura", "latitude", "longitude", "laudo"]
+
+class MedicamentoDataAdmin(ImportExportModelAdmin):
+    resource_class = TreeResource
+
+admin.site.register(Tree, MedicamentoDataAdmin)
--- habitas/main/models.py
+++ habitas/main/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Tree(models.Model):
+    N_placa = models.FloatField(default=0)
     nome_popular = models.CharField(max_length=255)
     nome_cientifico = models.CharField(max_length=255)
     dap = models.IntegerField()
     altura = models.IntegerField()
-    data_da_coleta = models.DateField()
+    #data_da_coleta = models.DateField(blank=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     laudo = models.URLField(max_length=255, blank=True)
